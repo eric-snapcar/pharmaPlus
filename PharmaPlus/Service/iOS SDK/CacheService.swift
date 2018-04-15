5eeb786abdaf3d045214b8ed6fcded6058c940a2
Eric Hth <eric@snapcar.com>
2018-04-15 18:33:25 +0200

    Service iOS SDK CacheService created

---
+++ PharmaPlus/Service/iOS SDK/CacheService.swift
@@ -0,0 +1,12 @@
+//
+//  CacheService.swift
+//  PharmaPlus
+//
+//  Created by Eric Hth on 15/04/2018.
+//  Copyright © 2018 Eric Hth. All rights reserved.
+//
+
+import Foundation
+class CacheService {
+    
+}
