56f86df9aa0e8861cb1c6c41bec656fad504b0b7
Eric Hth <eric@snapcar.com>
2018-04-16 13:44:52 +0200

    CacheService save() delete() ...

--- PharmaPlus/Service/iOS SDK/CacheService.swift
+++ PharmaPlus/Service/iOS SDK/CacheService.swift
@@ -14,3 +14,29 @@ class CacheService {
 }
 // CACHE EMAIL
 // CACHE PHOTO POST ID
+
+/*
+fileprivate class func save(_ value:AnyObject,key:SCServiceCacheValue){
+    let defaults = UserDefaults.standard
+    defaults.setValue(value, forKey: key.rawValue)
+    defaults.synchronize()
+}
+fileprivate class func get(_ key:SCServiceCacheValue) -> String?{
+    let defaults = UserDefaults.standard
+    return defaults.string(forKey: key.rawValue)
+}
+fileprivate class func getInt(_ key:SCServiceCacheValue) -> Int?{
+    let defaults = UserDefaults.standard
+    return defaults.integer(forKey: key.rawValue)
+}
+fileprivate class func getBool(_ key:SCServiceCacheValue) -> Bool?{
+    let defaults = UserDefaults.standard
+    return defaults.bool(forKey: key.rawValue)
+}
+fileprivate class func delete(_ key:SCServiceCacheValue) {
+    let defaults = UserDefaults.standard
+    defaults.removeObject(forKey: key.rawValue)
+    defaults.synchronize()
+}
+ */
+ 
