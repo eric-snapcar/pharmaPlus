4c549661289d7cdd24aedb8b6e006cea1ff4c984
Eric Hth <eric@snapcar.com>
2018-04-19 14:10:38 +0200

    AppDelegate ApplicationDidFinishLaunching() ... FirebaseService testLocation() ...

--- PharmaPlus/Service/Firebase/FirebaseService.swift
+++ PharmaPlus/Service/Firebase/FirebaseService.swift
@@ -21,6 +21,18 @@ extension DatabaseReference {
         let value = [ "timestamp":NSDate().timeIntervalSince1970] as [String : Any]
         newLocationRef.setValue(value)
     }
+    func checkIfUserExist( email : String, completion: @escaping ( _ exists: Bool ) -> Void ) -> Void {
+        let usersRef = self.child("users")
+        let email = "eric_hong_2000@yahoo.fr"
+        usersRef.queryOrdered(byChild: "email").queryEqual(toValue: email).observeSingleEvent(of: .value, with: { (snapshot) in
+            if(snapshot.exists()){
+                completion(true)
+            }
+            else {
+                completion(false)
+            }
+        })
+    }
     func addLocation( location : CLLocation ) -> Void {
         let locationRef = self.child("location")
         let newLocationRef = locationRef.childByAutoId()
